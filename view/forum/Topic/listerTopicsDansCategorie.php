e582b88e86c4be52c8cc3402b0db22b6ea5e4b74
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-04-26 16:48:25 +0200

    Ajout gestion du cas : 0 topic dans une catégorie

--- view/forum/Topic/listerTopicsDansCategorie.php
+++ view/forum/Topic/listerTopicsDansCategorie.php
@@ -7,9 +7,16 @@ $topics = $result["data"]['topics'];
 <h1>Liste topics dans la catégorie</h1>
 
 <?php
-foreach($topics as $topic){
+if($topics != NULL){
+    foreach($topics as $topic){
 
-    ?>
-    <p><a href="index.php?ctrl=post&action=listerPostsDansTopic&id=<?= $topic->getId() ?>"><?=$topic->getTitre() . " crée le " . $topic->getDateCreation()?></a></p>
-    <?php
+?>
+        <p><a href="index.php?ctrl=post&action=listerPostsDansTopic&id=<?= $topic->getId() ?>"><?=$topic->getTitre() . " crée le " . $topic->getDateCreation()?></a></p>
+<?php
+    }
+}
+else{
+?>
+    <p>Pas de topic dans cette catégorie !</p>
+<?php
 }
