58d1cf50f48cb4df215c82d455e585a4410b2043
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-04-27 10:57:40 +0200

    Ajustement syntaxe

--- view/forum/Post/listerPostsDansTopic.php
+++ view/forum/Post/listerPostsDansTopic.php
@@ -22,7 +22,7 @@ foreach($posts as $post){
 <form action="index.php?ctrl=post&action=ajouterPost&id=<?= $_GET["id"] ?>" method="post">
     <div>
         <label for="contenu">Contenu du message : </label>
-        <textarea name="contenu" id="contenu" cols="60" rows="10" require></textarea>
+        <textarea name="contenu" id="contenu" cols="60" rows="10" required></textarea>
     </div>
     <input type="submit" value="Ajouter le post" name="ajouterPost">
 </form>
