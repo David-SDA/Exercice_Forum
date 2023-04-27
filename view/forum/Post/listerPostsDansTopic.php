1fa95c1ec41e7d8d2f13414a7792b176f3edf1a8
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-04-27 10:27:55 +0200

    Ajout d'un formulaire d'ajout d'un post à un topic

--- view/forum/Post/listerPostsDansTopic.php
+++ view/forum/Post/listerPostsDansTopic.php
@@ -11,7 +11,18 @@ foreach($posts as $post){
 
     ?>
     <h3>Par : <?= $post->getMembre()->getPseudo() ?></h3>
+    <h5><?= $post->getDateCreation() ?></h5>
     <p><?=$post->getContenu()?></p>
 
     <?php
 }
+?>
+
+<h2>Ajouter un post</h2>
+<form action="" method="post">
+    <div>
+        <label for="contenu">Contenu du message : </label>
+        <textarea name="contenu" id="contenu" cols="60" rows="10" require></textarea>
+    </div>
+    <input type="submit" value="Ajouter le post">
+</form>
