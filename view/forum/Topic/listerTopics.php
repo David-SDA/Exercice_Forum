eb19ae4a7fb8f506aa59b5477ca3903842a7c88d
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-04-28 16:58:43 +0200

    Ajout début de style du listage des topics

--- view/forum/Topic/listerTopics.php
+++ view/forum/Topic/listerTopics.php
@@ -3,23 +3,41 @@
 $topics = $result["data"]['topics'];
     
 ?>
-
-<h1>Liste topics</h1>
-
-<?php
-if($topics != NULL){
-    foreach($topics as $topic){
-
-        ?>
-        <p><a href="index.php?ctrl=post&action=listerPostsDansTopic&id=<?= $topic->getId() ?>"><?=$topic->getTitre()?></a></p>
-<?php
+<div class="listeTopic">
+    <h1>LISTE DES TOPICS</h1>
+    <?php
+    if($topics != NULL){
+        foreach($topics as $topic){
+            ?>
+            <div class="unTopic">
+                <p><i><?= $topic->getMembre()->getPseudo() ?></i></p>
+                <div class="unTopicCentre">
+                    <a href="index.php?ctrl=post&action=listerPostsDansTopic&id=<?= $topic->getId() ?>"><?=$topic->getTitre()?></a>
+                    <p><i><?= $topic->getDateCreation() ?></i></p>
+                    <?php
+                        if($topic->getVerrouiller()){
+                        ?>
+                            <p><i class="fas fa-lock"></i></p>
+                        <?php
+                        }
+                        else{
+                            ?>
+                            <p><i class="fas fa-lock-open"></i></p>
+                            <?php
+                        }
+                    ?>
+                </div>
+                <p><i><?= $topic->getCategorie()->getNomCategorie() ?></i></p>
+            </div>
+    <?php
+        }
     }
-}
-else{
+    else{
+        ?>
+        <p>Il n'y a pas de topics !</p>
+    <?php
+    }
     ?>
-    <p>Il n'y a pas de topics !</p>
-<?php
-}
-?>
 
-    <a href="index.php?ctrl=categorie&action=allerPageAjoutTopic" class="lienAjout">AJOUTER UN TOPIC</a>
+        <a href="index.php?ctrl=categorie&action=allerPageAjoutTopic" class="lienAjout">AJOUTER UN TOPIC</a>
+</div>
