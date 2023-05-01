64a615cc80eb55116968d27e498f9b9c71a27ccf
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-05-01 12:08:41 +0200

    Ajustements style ajout catégorie

--- view/forum/Categorie/ajouterCategorie.php
+++ view/forum/Categorie/ajouterCategorie.php
@@ -1,8 +1,7 @@
-<h1>Ajouter une catégorie</h1>
-
-<form action="index.php?ctrl=categorie&action=ajouterCategorie" method="post">
+<form action="index.php?ctrl=categorie&action=ajouterCategorie" method="post" class="formCategorie">
+    <h1><i>AJOUTER UNE CATÉGORIE</i></h1>
     <label for="nomCategorie">Nom de la catégorie : </label>
     <input type="text" name="nomCategorie" id="nomCategorie" required>
 
-    <input type="submit" value="Ajouter la catégorie" name="submitCategorie" class="boutonAjout">
+    <input type="submit" value="AJOUTER UNE CATÉGORIE" name="submitCategorie" class="lienAjout ajoutFormulaire grand">
 </form>
