19517b64ae5ab53a16a152300df309454c818f9d
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-05-01 12:01:36 +0200

    Ajustements style ajout d'un post

--- view/forum/Post/ajouterPost.php
+++ view/forum/Post/ajouterPost.php
@@ -1,9 +1,9 @@
 <a href="index.php?ctrl=post&action=listerPostsDansTopic&id=<?= $_GET["id"] ?>" class="retourTopic"><span><i class="fas fa-arrow-left"></i></span>Retour au topic</a>
-<h2>Ajouter un post</h2>
 <form action="index.php?ctrl=post&action=ajouterPost&id=<?= $_GET["id"] ?>" method="post">
+    <h1>AJOUTER UN POST</h1>
     <div>
-        <label for="contenu">Contenu du message : </label>
-        <textarea name="contenu" id="contenu" cols="60" rows="10" required></textarea>
+        <label for="contenu">Contenu du post : </label>
+        <textarea name="contenu" id="contenu" cols="60" rows="16" required></textarea>
     </div>
-    <input type="submit" value="Ajouter le post" name="ajouterPost" class="boutonAjout">
+    <input type="submit" value="AJOUTER UN POST" name="ajouterPost" class="lienAjout ajoutFormulaire">
 </form>
