2a4bbcd4348bba94de6e0b59beff932eff837a42
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-05-01 11:21:29 +0200

    Ajout style de la liste des catégories

--- view/forum/Categorie/listerCategories.php
+++ view/forum/Categorie/listerCategories.php
@@ -4,22 +4,22 @@ $categories = $result["data"]['categories'];
     
 ?>
 
-<h1>Liste catégories</h1>
+<div class="listeTopic">
+    <h1>CATÉGORIES</h1>
+    <?php
+    if($categories != NULL){
+        foreach($categories as $categorie){
 
-<?php
-if($categories != NULL){
-    foreach($categories as $categorie){
-
-        ?>
-        <p><a href="index.php?ctrl=topic&action=listerTopicsDansCategorie&id=<?= $categorie->getId() ?>"><?=$categorie->getNomCategorie()?></a></p>
-        <?php
+            ?>
+            <p class="uneCategorie"><a href="index.php?ctrl=topic&action=listerTopicsDansCategorie&id=<?= $categorie->getId() ?>"><?=$categorie->getNomCategorie()?></a></p>
+            <?php
+        }
     }
-}
-else{
+    else{
+        ?>
+        <p>Il n'y a pas de catégories !</p>
+    <?php    
+    }
     ?>
-    <p>Il n'y a pas de catégories !</p>
-<?php    
-}
-?>
-
-    <a href="index.php?ctrl=categorie&action=allerPageAjoutCategorie" class="lienAjout">AJOUTER UNE CATÉGORIE</a>
+</div>
+<a href="index.php?ctrl=categorie&action=allerPageAjoutCategorie" class="lienAjout grand">AJOUTER UNE CATÉGORIE</a>
