5d1281f3de82d00c60c2bf70746d2179e5dafdce
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-05-01 10:28:42 +0200

    Ajustement, ajout post en fonction du verrouillage

--- view/forum/Post/listerPostsDansTopic.php
+++ view/forum/Post/listerPostsDansTopic.php
@@ -1,14 +1,16 @@
 <?php
 
-$posts = $result["data"]['posts'];
+$posts = $result["data"]["posts"];
     
 ?>
 
 <h1>Liste posts du topic</h1>
 
 <?php
+$verrouiller = 0;
 if($posts != NULL){
     foreach($posts as $post){
+        $verrouiller = $post->getTopic()->getVerrouiller();
 
         ?>
         <h3>Par : <?= $post->getMembre()->getPseudo() ?></h3>
@@ -23,6 +25,10 @@ else{
     <p>Il n'y a pas post dans le topic</p>
 <?php
 }
-?>
 
-<a href="index.php?ctrl=post&action=allerPageAjoutPost&id=<?= $_GET["id"] ?>" class="lienAjout">AJOUTER UN POST</a>
+if(!$verrouiller){
+?>
+    <a href="index.php?ctrl=post&action=allerPageAjoutPost&id=<?= $_GET["id"] ?>" class="lienAjout">AJOUTER UN POST</a>
+<?php
+}
+?>
