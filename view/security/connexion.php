0083d695310488d81e73272f7d1e35176b9cae08
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-05-02 11:22:47 +0200

    Ajout base du formulaire de connexion

--- view/security/connexion.php
+++ view/security/connexion.php
@@ -1,1 +1,12 @@
-<h1>test</h1>
+<form action="#" method="post">
+    <h1><i>SE CONNECTER</i></h1>
+    <div>
+        <label for="email">Email : </label>
+        <input type="email" name="email" id="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required>
+    </div>
+    <div>
+        <label for="motDePasse">Mot de passe :</label>
+        <input type="password" name="motDePasse" id="motDePasse">
+    </div>
+    <input type="submit" value="SE CONNECTER" name="submitConnexion" class="lienAjout ajoutFormulaire">
+</form>
