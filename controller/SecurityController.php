1173b71cf1ed95c7f45fbe821c1ee3fe6bd2a4cf
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-05-02 11:06:55 +0200

    Ajout de quelques commentaires

--- controller/SecurityController.php
+++ controller/SecurityController.php
@@ -12,32 +12,42 @@
         
         public function index(){}
 
+        /**
+         * Permet d'aller à la page d'inscription
+         */
         public function allerPageInscription(){
             return ["view" => VIEW_DIR . "security/inscription.php"];
         }
 
+        /**
+         * Permet de s'incrire
+         */
         public function inscription(){
-            $sessionManager = new Session();
-            $membreManager = new MembreManager();
-            $topicManager = new TopicManager();
+            $sessionManager = new Session(); // Pour pouvoir utiliser le message flash
+            $membreManager = new MembreManager(); // Pour la gestion de la base de données membre
+            $topicManager = new TopicManager(); // En option, le temps de faire le formulaire de connexion
 
+            /* Filtrage des variables post */
             $pseudo = filter_input(INPUT_POST, "pseudo", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
             $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
             $motDePasse = filter_input(INPUT_POST, "motDePasse", FILTER_SANITIZE_SPECIAL_CHARS);
             $motDePasseConfirmation = filter_input(INPUT_POST, "motDePasseConfirmation", FILTER_SANITIZE_SPECIAL_CHARS);
             
+            /* Si l'email existe, on indique visuellement que celui-ci existe déjà et on le redirige vers le formulaire d'inscription */
             if($membreManager->trouverEmail($email)){
                 $sessionManager->addFlash("error", "L'email saisit existe déjà ! Saisissez-en un autre !");
                 return [
                     "view" => VIEW_DIR."security/inscription.php",
                 ];
             }
+            /* Si le pseudo existe, on indique visuellement que celui-ci existe déjà et on le redirige vers le formulaire d'inscription  */
             if($membreManager->trouverPseudo($pseudo)){
                 $sessionManager->addFlash("error", "Le pseudo saisit existe déjà ! Saisissez-en un autre !");
                 return [
                     "view" => VIEW_DIR."security/inscription.php",
                 ];
             }
+            /* Lorsque les deux mots de passe ne sont pas identiques, on l'indique visuellement et on le redirige vers le formulaire d'inscription */
             if($motDePasse != $motDePasseConfirmation){
                 $sessionManager->addFlash("error", "Les mots de passe ne sont pas identiques ! Veuillez les saisirs à nouveau !");
                 return [
@@ -45,8 +55,12 @@
                 ];
             }
 
+            /* On hash le mot de passe */
             $motDePasseHash = password_hash($motDePasse, PASSWORD_DEFAULT);
 
+            /* On ajoute le membre :
+            si cela fonctionne, on redirige l'utilisateur vers le formulaire de connexion
+            sinon on le redirige vers le formulaire d'inscription */
             if($membreManager->add([
                 "pseudo" => $pseudo,
                 "email" => $email,
