6116c26d7d6f71605e10d6a8c88b28cac045767f
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-05-02 09:07:30 +0200

    Création d'un formulaire d'inscription

--- view/security/inscription.php
+++ view/security/inscription.php
@@ -1,1 +1,20 @@
-<h1>test</h1>
+<form action="#">
+    <h1><i>S'INSCRIRE</i></h1>
+    <div>
+        <label for="pseudo">Pseudo :</label>
+        <input type="text" name="pseudo" id="pseudo" required>
+    </div>
+    <div>
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required>
+    </div>
+    <div>
+        <label for="motDePasse">Mot de passe :</label>
+        <input type="password" name="motDePasse" id="motDePasse" required>
+    </div>
+    <div>
+        <label for="motDePasseConfirmation">Confirmation du mot de passe :</label>
+        <input type="password" name="motDePasseConfimation" id="motDePasseConfirmation" required>
+    </div>
+    <input type="submit" value="S'INSCRIRE" name="submitInscription" class="lienAjout ajoutFormulaire">
+</form>
