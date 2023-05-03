48c7719399d2f4fb276b036d2995bda21944d062
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-05-03 14:22:32 +0200

    Ajout de commentaires

--- controller/TopicController.php
+++ controller/TopicController.php
@@ -19,7 +19,7 @@
             return [
                 "view" => VIEW_DIR."forum/Topic/listerTopics.php",
                 "data" => [
-                    "topics" => $topicManager->findAll(["dateCreation", "DESC"])
+                    "topics" => $topicManager->findAll(["dateCreation", "DESC"]) // On cherche tout les topic trier du plus récent au plus ancien
                 ]
             ];
         }
@@ -29,7 +29,10 @@
          */
         public function listerTopicsDansCategorie(){
             $topicManager = new TopicManager();
+
+            /* On filtre l'input */
             $id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
             return [
                 "view" => VIEW_DIR . "forum/Topic/listerTopicsDansCategorie.php",
                 "data" => [
@@ -44,26 +47,30 @@
         public function ajouterTopic(){
             $topicManager = new TopicManager();
             $postManager = new PostManager();
+            $sessionManager = new Session();
 
+            /* On filtre les inputs */
             $titre = filter_input(INPUT_POST, "titre", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
             $categorie = filter_input(INPUT_POST, "categorie", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
             $contenu = filter_input(INPUT_POST, "contenu", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
-            $sessionManager = new Session();
-            $id = $topicManager->add([
-                "titre" => $titre,
-                "membre_id" => Session::getUser()->getId(),
-                "categorie_id" => $categorie
-            ]);
-            if($id && $postManager->add([
-                "contenu" => $contenu,
-                "membre_id" => Session::getUser()->getId(),
-                "topic_id" => $id
-            ])){
-                $sessionManager->addFlash("success", "Ajout réussi !");
-            }
-            else{
-                $sessionManager->addFlash("error", "Echec de l'ajout !");
+            /* Si le filtrage fonctionne */
+            if($titre && $categorie && $contenu){
+                $id = $topicManager->add([
+                    "titre" => $titre,
+                    "membre_id" => Session::getUser()->getId(),
+                    "categorie_id" => $categorie
+                ]); // On ajoute un topic et on récupère son id
+                if($id && $postManager->add([
+                    "contenu" => $contenu,
+                    "membre_id" => Session::getUser()->getId(),
+                    "topic_id" => $id
+                ])){// On ajoute le premier post du topic
+                    $sessionManager->addFlash("success", "Ajout réussi !");
+                }
+                else{
+                    $sessionManager->addFlash("error", "Echec de l'ajout !");
+                }
             }
             return [
                 "view" => VIEW_DIR."forum/Topic/listerTopics.php",
@@ -73,14 +80,18 @@
             ];
         }
 
+        /* Permet de supprimer un topic avec ses posts inclues */
         public function supprimerTopic(){
             $topicManager = new TopicManager();
             $postManager = new PostManager();
             $session = new Session();
 
+            /* On filtre l'input */
             $id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            
+            /* Si le filtrage fonctionne */
             if($id){
-                if($postManager->supprimerPostsDuTopic($id) && $topicManager->delete($id)){
+                if($postManager->supprimerPostsDuTopic($id) && $topicManager->delete($id)){ // On supprime les posts du topic puis le topic
                     $session->addFlash("success", "Suppression réussi !");
                 }
                 else{
