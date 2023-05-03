8492eb32d974fd316193ee1e6d132565e7e06132
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-05-03 11:54:54 +0200

    Ajout de la liste détaillé des membres

--- view/security/listeMembres.php
+++ view/security/listeMembres.php
@@ -1,10 +1,22 @@
-<h1><i>LISTE DES MEMBRES</i></h1>
 <?php
 $membres = $result["data"]["membres"];
-
-foreach($membres as $membre){
-    ?>
-    <p><?= $membre->getPseudo() ?></p>
-    <?php
-}
 ?>
+<div class="liste">
+    <h1><i>LISTE DES MEMBRES</i></h1>
+    <div class="element bordBas">
+        <p class="elementGauche centre"><i><b>DATE D'INSCRIPTION</b></i></p>
+        <p class="elementCentre"><i><b>PSEUDO</b></i></p>
+        <p class="elementDroite centre"><i><b>RÔLE</b></i></p>
+    </div>
+    <?php
+    foreach($membres as $membre){
+        ?>
+        <div class="element">
+            <p class="elementGauche centre"><?= $membre->getDateInscription() ?></p>
+            <p class="elementCentre"><b><?= $membre->getPseudo() ?></b></p>
+            <p class="elementDroite centre"><?= $membre->getRole() ?></p>
+        </div>
+        <?php
+    }
+    ?>
+</div>
