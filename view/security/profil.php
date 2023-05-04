602e85a2eda8d5c86889189aaf8557b326a08e3f
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-05-04 15:45:56 +0200

    Ajustements style du profil

--- view/security/profil.php
+++ view/security/profil.php
@@ -1,12 +1,13 @@
 <?php
 if(App\Session::getUser()){
     ?>
-    <h1>Votre profil</h1>
-
-    <p><b><i>Pseudo : </i></b><?= App\Session::getUser()->getPseudo() ?></p>
-    <p><b><i>Email : </i></b><?= App\Session::getUser()->getEmail() ?></p>
-    <p><b><i>Date d'inscription : </i></b><?= App\Session::getUser()->getDateInscription() ?></p>
-    <p><b><i>Nombre de topics : </i></b><?= $result["data"]["nombreTopics"] ?></p>
-    <p><b><i>Nombre de posts : </i></b><?= $result["data"]["nombrePosts"] ?></p>
+    <h1><i>Votre profil</i></h1>
+    <div class="detailProfil">
+        <p><b><i>Pseudo : </i></b><?= App\Session::getUser()->getPseudo() ?></p>
+        <p><b><i>Email : </i></b><?= App\Session::getUser()->getEmail() ?></p>
+        <p><b><i>Date d'inscription : </i></b><?= App\Session::getUser()->getDateInscription() ?></p>
+        <p><b><i>Nombre de topics : </i></b><?= $result["data"]["nombreTopics"] ?></p>
+        <p><b><i>Nombre de posts : </i></b><?= $result["data"]["nombrePosts"] ?></p>
+    </div>
     <?php
 }
