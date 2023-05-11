02f389db8fe4e3c7edb51a3e92840f999f76002e
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-05-11 09:34:10 +0200

    Ajout bouton retour topic, modification d'un post

--- view/forum/Post/modifierPost.php
+++ view/forum/Post/modifierPost.php
@@ -1,6 +1,7 @@
 <?php
 $post = $result["data"]["post"];
 ?>
+<a href="index.php?ctrl=post&action=listerPostsDansTopic&id=<?= $post->getTopic()->getId() ?>" class="retourTopic"><span><i class="fas fa-arrow-left"></i></span>Retour au topic</a>
 <form action="index.php?ctrl=post&action=modificationPost&id=<?= $post->getId() ?>" method="post">
     <h1><i>MODIFIER LE POST</i></h1>
     <div>
