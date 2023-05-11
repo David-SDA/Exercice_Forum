dde2c05626edf0a5885999d6532017b75a315768
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-05-11 14:58:18 +0200

    Ajout fonction,trouver catégories avec nombre post

--- model/managers/CategorieManager.php
+++ model/managers/CategorieManager.php
@@ -14,5 +14,22 @@
             parent::connect();
         }
 
+        /**
+         * Permet de trouver toute les catégories avec le nombre de topic dans celui-ci
+         */
+        public function trouverCategorieAvecNombreTopic($order = null){
+            $orderQuery = ($order) ? "ORDER BY " . $order[0] . " " . $order[1] : "";
+            
+            $requete = "SELECT *, (SELECT COUNT(topic.id_topic)
+                                FROM topic
+                                WHERE topic." . $this->tableName . "_id = id_" . $this->tableName . "
+                                ) AS nombreTopic
+                    FROM " . $this->tableName . " a
+                    ". $orderQuery;
+            return $this->getMultipleResults(
+                DAO::select($requete),
+                $this->className
+            );
+        }
 
     }
